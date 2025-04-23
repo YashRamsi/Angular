0e3cfb175e2c5704a8589c9e100ea7c652359257
YashRamsi <yaswanthchowdary.pavuluri@gmail.com>
2025-04-23 13:53:54 -0400

    Day 3 Workings

--- javascript/day_02_april_22/js_add_demo_1.js
+++ javascript/day_02_april_22/js_add_demo_1.js
@@ -4,4 +4,4 @@ console.log(`Addition of $a and $b is ${a + b}`); // String literals
 console.log("Multiplication of " + a + " and " + b + " is " + (a * b)); // String concatenation
 
 
-swindow.alert("Hello World"); // Alert box //Runs only on browser not in nodeJS
+//window.alert("Hello World"); // Alert box //Runs only on browser not in nodeJS
